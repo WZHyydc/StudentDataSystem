902b58c9ca28ee8ed373f981eefce64bdb49f780
wzhyydc <1479882757@qq.com>
2022-05-19 16:31:54 +0800

    Initial commit

---
+++ StudentSystem/src/StudentSystem/dao/JdbcConfig.java
@@ -0,0 +1,8 @@
+package StudentSystem.dao;
+
+public interface JdbcConfig {
+    String DRIVER = "com.mysql.jdbc.Driver";
+    String URL = "jdbc:mysql://localhost:3306/StudentSystemDao?useUnicode=true&characterEncoding=UTF8&uesSSL=false";
+    String USERNAME = "root";
+    String PASSWORD = "wzh2003521*";
+}

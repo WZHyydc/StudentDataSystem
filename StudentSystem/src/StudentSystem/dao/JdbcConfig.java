7a15f9cdd5d16f8e1afca4cc365b6379c788446f
WZHyydc <105038591+WZHyydc@users.noreply.github.com>
2022-05-20 10:00:32 +0800

    Update JdbcConfig.java

--- StudentSystem/src/StudentSystem/dao/JdbcConfig.java
+++ StudentSystem/src/StudentSystem/dao/JdbcConfig.java
@@ -3,6 +3,7 @@ package StudentSystem.dao;
 public interface JdbcConfig {
     String DRIVER = "com.mysql.jdbc.Driver";
     String URL = "jdbc:mysql://localhost:3306/StudentSystemDao?useUnicode=true&characterEncoding=UTF8&uesSSL=false";
-    String USERNAME = "root";
-    String PASSWORD = "wzh2003521*";
+    String USERNAME = "";
+    String PASSWORD = "";
+//     这里输入您的数据库用户名和密码
 }
